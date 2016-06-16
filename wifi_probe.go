e2407111cac840dac5c67bcb4811082c8f520865
roy.lieu <roy.lieu@gmail.com>
2016-06-16 10:45:12 +0800

    go fromat for wifi probe

--- wifi_probe.go
+++ wifi_probe.go
@@ -6,12 +6,12 @@ import (
 	"fmt"
 	"net"
 	"syscall"
-    "unsafe"
+	"unsafe"
 )
 
 type afpacket struct {
-	ifce   *net.Interface
-	fd int
+	ifce       *net.Interface
+	fd         int
 	sockaddrLL *syscall.SockaddrLinklayer
 }
 
@@ -32,15 +32,15 @@ func newDev(ifce *net.Interface) (*afpacket, error) {
 	d := new(afpacket)
 	d.ifce = ifce
 
-    d.fd, err = syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, htons(0x0003))
+	d.fd, err = syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, htons(0x0003))
 	if err != nil {
 		return d, err
 	}
 
 	d.sockaddrLL = new(syscall.SockaddrLinklayer)
 	d.sockaddrLL.Ifindex = ifce.Index
-    d.sockaddrLL.Protocol = uint16(htons(0x0003))
-    syscall.Bind(d.fd, d.sockaddrLL)
+	d.sockaddrLL.Protocol = uint16(htons(0x0003))
+	syscall.Bind(d.fd, d.sockaddrLL)
 
 	return d, err
 }
@@ -54,35 +54,35 @@ func (d *afpacket) Close() error {
 }
 
 func (d *afpacket) Read(to []byte) error {
-    _, _, err := syscall.Recvfrom(d.fd, to, 0)
-    if err != nil {
-        return err
-    }
-    return nil
+	_, _, err := syscall.Recvfrom(d.fd, to, 0)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func HandleFrame(frame []byte) {
-    lens := int(frame[2])
+	lens := int(frame[2])
 
-    // beacon frame
-    if frame[lens] == 0x80 {
-        mac := frame[ lens+10 : lens+16]
-        ssid := frame[ lens+38 : (lens+38+int(frame[lens+37])) ]
-        mac_str := fmt.Sprintf("%x:%x:%x:%x:%x:%x", int(mac[0]), int(mac[1]), int(mac[2]), int(mac[3]), int(mac[4]), int(mac[5]))
-        ssid_str := string(ssid)
-        fmt.Printf("MAC: %s, SSID: %s\n", mac_str, ssid_str)
-    }
+	// beacon frame
+	/*
+		if frame[lens] == 0x80 {
+			mac := frame[lens+10 : lens+16]
+			ssid := frame[lens+38 : (lens + 38 + int(frame[lens+37]))]
+			mac_str := fmt.Sprintf("%x:%x:%x:%x:%x:%x", int(mac[0]), int(mac[1]), int(mac[2]), int(mac[3]), int(mac[4]), int(mac[5]))
+			ssid_str := string(ssid)
+			fmt.Printf("MAC: %s, SSID: %s\n", mac_str, ssid_str)
+		}
+	*/
 
-    /*
-    // probe request frame
-    if frame[lens] == 0x40 {
-        mac := frame[ lens+10 : lens+16]
-        ssid := frame[ lens+26: (lens+26+int(frame[lens+25])) ]
-        mac_str := fmt.Sprintf("%x:%x:%x:%x:%x:%x", int(mac[0]), int(mac[1]), int(mac[2]), int(mac[3]), int(mac[4]), int(mac[5]))
-        ssid_str := string(ssid)
-        fmt.Printf("MAC: %s, SSID: %s\n", mac_str, ssid_str)
-    }
-    */
+	// probe request frame
+	if frame[lens] == 0x40 {
+		mac := frame[lens+10 : lens+16]
+		ssid := frame[lens+26 : (lens + 26 + int(frame[lens+25]))]
+		mac_str := fmt.Sprintf("%x:%x:%x:%x:%x:%x", int(mac[0]), int(mac[1]), int(mac[2]), int(mac[3]), int(mac[4]), int(mac[5]))
+		ssid_str := string(ssid)
+		fmt.Printf("MAC: %s, SSID: %s\n", mac_str, ssid_str)
+	}
 }
 
 func main() {
@@ -98,14 +98,14 @@ func main() {
 		return
 	}
 
-    for {
-        frame := make([]byte, 2048)
-        err := dev.Read(frame)
-        if err != nil {
-            fmt.Println(err)
-            continue
-        }
-        HandleFrame(frame)
-    }
+	for {
+		frame := make([]byte, 2048)
+		err := dev.Read(frame)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		HandleFrame(frame)
+	}
 
 }
