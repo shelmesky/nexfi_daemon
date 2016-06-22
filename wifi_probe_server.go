2b1cf5e0906ecfde8cf718217d1548c7b6be7b45
roy.lieu <roy.lieu@gmail.com>
2016-06-22 15:50:42 +0800

    add mysql processor

--- wifi_probe_server.go
+++ wifi_probe_server.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/gob"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"io"
 	"log"
 	"net"
 	"os"
+	"time"
+)
+
+var (
+	db *sql.DB
 )
 
 type Client struct {
@@ -14,6 +21,38 @@ type Client struct {
 	RSSI   int
 	SSID   string
 	Action int
+}
+
+func (this *Client) Insert() {
+	stmtIns, err := db.Prepare("INSERT INTO clients VALUES(?, ?, ?, ?, ?, ?, ?)")
+	if err != nil {
+		log.Println("can not do db.Prepare:", err)
+		return
+	}
+	defer stmtIns.Close()
+
+	now_timestamp := time.Now().Unix()
+	now_timestring := time.Now().Format("2006-01-02 15:04:05")
+	_, err = stmtIns.Exec(nil, this.Addr, this.RSSI, this.SSID, this.Action, now_timestamp, now_timestring)
+	if err != nil {
+		log.Println("can not do stmt.Exec:", err)
+	}
+}
+
+func init() {
+	var err error
+
+	db, err = sql.Open("mysql", "root:4974481@tcp(121.199.74.47:3306)/wifi_probe")
+	if err != nil {
+		log.Println("failed connect to mysql:", err)
+		os.Exit(1)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Println("failed ping mysql server:", err)
+		os.Exit(1)
+	}
 }
 
 func HandleConnection(conn net.Conn) {
@@ -31,6 +70,7 @@ func HandleConnection(conn net.Conn) {
 			break
 		}
 		log.Println("got client data:", client)
+		client.Insert()
 	}
 }
 
